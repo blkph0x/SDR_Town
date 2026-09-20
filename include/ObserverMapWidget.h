d8bc743cd0425b5dd3f558e8e3be6b9a26877eaa
BlkPh0x <blkph0x@gmail.com>
2026-09-20 23:05:05 +1000

    satcom map: add selected satellite markers

--- include/ObserverMapWidget.h
+++ include/ObserverMapWidget.h
@@ -3,18 +3,35 @@
 #include <QHash>
 #include <QPixmap>
 #include <QPointF>
+#include <QString>
 #include <QWidget>
+
+#include <vector>
 
 class QNetworkAccessManager;
 class QLabel;
 
-// Click-to-pick OSM map for the satcom/ISS home observer (both hemispheres).
+struct SatelliteMapMarker {
+    QString id;
+    QString name;
+    double latDeg = 0.0;
+    double lonDeg = 0.0;
+    double altitudeKm = 0.0;
+    double elevationDeg = -90.0;
+    bool valid = false;
+    bool inRange = false;
+    bool armed = false;
+};
+
+// Click-to-pick OSM map for the satcom/ISS home observer (both hemispheres),
+// with live propagated markers for every selected satellite.
 class ObserverMapWidget : public QWidget {
     Q_OBJECT
 public:
     explicit ObserverMapWidget(QWidget* parent = nullptr);
 
     void setMarker(double latDeg, double lonDeg);
+    void setSatellites(const std::vector<SatelliteMapMarker>& satellites);
     double markerLat() const { return markerLat_; }
     double markerLon() const { return markerLon_; }
 
@@ -29,6 +46,8 @@ protected:
 
 private:
     void ensureTiles();
+    void fitSatelliteOverview();
+    bool satelliteMarkersVisible() const;
     QPointF latLonToPixel(double lat, double lon) const;
     bool pixelToLatLon(const QPointF& pt, double* lat, double* lon) const;
 
@@ -41,4 +60,6 @@ private:
     QNetworkAccessManager* nam_ = nullptr;
     QHash<QString, QPixmap> tiles_;
     QLabel* hint_ = nullptr;
+    std::vector<SatelliteMapMarker> satellites_;
+    QString satelliteSignature_;
 };
